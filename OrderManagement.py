7c906a37be1b9eb85fecad3bfd9ba88caea2bf62
Moneebriaz <83026778+Moneebriaz@users.noreply.github.com>
2022-08-12 19:53:25 +0000

    Update OrderManagement.py

--- OrderManagement.py
+++ OrderManagement.py
@@ -1,16 +1,25 @@
+# MySQL Library to connect database
 import mysql.connector
+# Tkinter library for GUI
 from tkinter import *
+# Pandas library for dataframe
 import pandas as pd
+# PandasTable for table
 from pandastable import Table, TableModel
 
 
+# Establish database connection
 conn = mysql.connector.connect(
     host="localhost", user="root", password="", database="ordermanagement")
+# Cursor to run queries
 cursor = conn.cursor()
+
+# function to call on click for menu option
 
 
 def OptionMenu_SelectionEvent(*args):
     option = variable.get()
+# calling SQL queries according to option selected
     if (option == "Get List of customers with their payments"):
         cursor.execute(
             "SELECT c.customerName, SUM(p.amount) AS amount FROM payments p, customers c WHERE p.customerNumber = c.customerNumber GROUP BY p.customerNumber")
@@ -26,17 +35,23 @@ def OptionMenu_SelectionEvent(*args):
     elif (option == "List customers whom orders are on hold"):
         cursor.execute(
             "SELECT * FROM customers WHERE customerNumber IN (SELECT customerNumber FROM orders WHERE status = \"On Hold\")")
+# fetching all results
     values = cursor.fetchall()
+# getting column names
     columns = [col[0] for col in cursor.description]
+# Creating dataframe
     df = pd.DataFrame(values, columns=columns)
     df.head()
+# Creating frame for table
     f = Frame(gui)
     # f.pack(fill=BOTH,expand=1)
+# creating table with dataframe and adding it to frame
     table = pt = Table(f, dataframe=df, showtoolbar=True, showstatusbar=True)
     pt.show()
     f.grid(row=3, column=0)
 
 
+# List for options
 OPTIONS = [
     "Get List of customers with their payments",
     "List customer with median payment",
@@ -45,9 +60,12 @@ OPTIONS = [
     "List customers whom orders are on hold"
 ]
 
+# GUI root for adding widgets
 gui = Tk()
+# Setting title
 gui.title("Order Management System")
-gui.geometry("850x800")
+# Setting Geometry size
+gui.geometry("650x600")
 
 l = Label(gui, text="Order Management System")
 l.config(font=("Courier", 32))
@@ -64,6 +82,8 @@ w = OptionMenu(gui, variable, *OPTIONS, command=OptionMenu_SelectionEvent)
 w.config(font=("Courier", 14))
 w.grid(row=2, column=0)
 
+# to display screen
 gui.mainloop()
 
+# close database connection
 conn.close()
